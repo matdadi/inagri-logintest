29982239eeb8249a8dd6e4501731edfb2dd93a12
matdadi <49846800+matdadi@users.noreply.github.com>
2022-09-05 11:31:04 +0700

    Update __init__.py

--- dadi-submission/__init__.py
+++ dadi-submission/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask
-from flask_mysqldb import MySQL
 
 def create_app():
     app = Flask(__name__)
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'dadi-submission'
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
-    db = MySQL(app)
-    db.init_app(app)
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
